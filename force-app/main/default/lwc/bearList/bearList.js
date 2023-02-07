4d6e2565d2532cba78bc836feaec80449387aa13
mugheeskhan1 <mugheeskhan36@gmail.com>
2023-02-07 17:27:59 +0500

    made bearTiles clickable & opens record form

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,10 +1,9 @@
+import { NavigationMixin } from 'lightning/navigation';
 import { LightningElement, wire } from 'lwc';
-import ursusResources from '@salesforce/resourceUrl/ursus_park';
 
 /** BearController.searchBears(searchTerm) Apex method */
 import searchBears from '@salesforce/apex/BearController.searchBears';
-
-export default class BearList extends LightningElement {
+export default class BearList extends NavigationMixin(LightningElement) {
     searchTerm = '';
 
     @wire(searchBears, { searchTerm: '$searchTerm' })
@@ -24,5 +23,19 @@ export default class BearList extends LightningElement {
 
     get hasResults() {
         return (this.bears.data.length > 0);
+    }
+
+    handleBearView(event) {
+        // Get bear record id from bearview event
+        const bearId = event.detail;
+        // Navigate to bear record page
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: bearId,
+                objectApiName: 'Bear__c',
+                actionName: 'view',
+            },
+        });
     }
 }
--- force-app/main/default/lwc/bearTile/bearTile.js
+++ force-app/main/default/lwc/bearTile/bearTile.js
@@ -8,4 +8,10 @@ export default class BearTile extends LightningElement {
         bearSilhouette: `${ursusResources}/standing-bear-silhouette.png`,
     };
 
+    handleOpenRecordClick() {
+        const selectEvent = new CustomEvent('bearview', {
+            detail: this.bear.Id
+        });
+        this.dispatchEvent(selectEvent);
+    }
 }
