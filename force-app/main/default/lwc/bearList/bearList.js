10196d166c4280a2213e0ac791e113d7f92a8dcf
mugheeskhan1 <mugheeskhan36@gmail.com>
2023-02-07 17:57:36 +0500

    bearMap component created ( used messageChannel)

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -3,11 +3,23 @@ import { LightningElement, wire } from 'lwc';
 
 /** BearController.searchBears(searchTerm) Apex method */
 import searchBears from '@salesforce/apex/BearController.searchBears';
+import { publish, MessageContext } from 'lightning/messageService';
+import BEAR_LIST_UPDATE_MESSAGE from '@salesforce/messageChannel/BearListUpdate__c';
 export default class BearList extends NavigationMixin(LightningElement) {
     searchTerm = '';
 
-    @wire(searchBears, { searchTerm: '$searchTerm' })
     bears;
+    @wire(MessageContext) messageContext;
+    @wire(searchBears, { searchTerm: '$searchTerm' })
+    loadBears(result) {
+        this.bears = result;
+        if (result.data) {
+            const message = {
+                bears: result.data
+            };
+            publish(this.messageContext, BEAR_LIST_UPDATE_MESSAGE, message);
+        }
+    }
 
     handleSearchTermChange(event) {
         // Debouncing this method: do not update the reactive property as
